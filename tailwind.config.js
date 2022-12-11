d6fd650e6ef9ec0dcb90bba2199ae9705e702008
m0bad <mobad.cai@gmail.com>
2022-12-11 23:02:43 +0200

    fix tailwind deploy issue attempt #2.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     content: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
-    ],
-    purge: [
-        './pages/**/*.{js,ts,jsx,tsx}',
-        './components/**/*.{js,ts,jsx,tsx}',
+        './Pages/**/*.{js,ts,jsx,tsx}',
+        './Components/**/*.{js,ts,jsx,tsx}',
     ],
     theme: {
         extend: {
