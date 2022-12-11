3c4a2cccb920141f7ea6f672a0ddd78f2d617de7
m0bad <mobad.cai@gmail.com>
2022-12-11 08:08:25 +0200

    installed tailwind.

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
+    theme: {
+        extend: {
+            colors: {
+                gray: {
+                    100: '#F5F5F5',
+                    300: '#F8F8F8',
+                    500: '#E0E0DF',
+                    700: '#959595',
+                    900: '#616161',
+                },
+                table: {
+                    primary: '#1C1C1C',
+                },
+            },
+        },
+    },
+    plugins: [],
+}
