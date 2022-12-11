cc9937c99f2fedf079bd5adc44c85a9194b98569
m0bad <mobad.cai@gmail.com>
2022-12-11 22:55:07 +0200

    fix tailwind deploy issue attempt.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,10 @@ module.exports = {
         './pages/**/*.{js,ts,jsx,tsx}',
         './components/**/*.{js,ts,jsx,tsx}',
     ],
+    purge: [
+        './pages/**/*.{js,ts,jsx,tsx}',
+        './components/**/*.{js,ts,jsx,tsx}',
+    ],
     theme: {
         extend: {
             colors: {
